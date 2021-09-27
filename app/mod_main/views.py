b8ad77604f6aa8509a6aa5f8e0293d568a2a74e4
Andrei <mr.andreistirb@gmail.com>
2021-09-27 23:24:26 +0300

    adding privacy policy

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -3,6 +3,7 @@
 from app.token import confirm_token, generate_confirmation_token
 from app.email import send_email
 from flask import render_template, Blueprint, request, flash, redirect, url_for
+from flask import Markup
 
 from app.mod_main.forms import SignUpForm
 from app import db
@@ -20,21 +21,28 @@ def index():
     if form.validate_on_submit():
         test = Email.query.filter_by(email=form.email.data, source=None).first()
         if test:
-            flash('Sorry that email aleady exists!', 'danger')
+            # check if email is confirmed
+            if test.confirmed:
+                flash('You have already subscribed, thank you!', 'success')
+            else:
+                token = generate_confirmation_token(form.email.data)
+                confirm_url = url_for('main.confirm_email', token=token, _external=True)
+                html = render_template('email.html', confirm_url=confirm_url)
+                subject = "Please confirm your subscription to ImgUpscale.com"
+                send_email(form.email.data, subject, html)
+                flash(Markup("email not confirmed, we have sent you another mail for confirmation"), 'success')
         else:
             email = Email(email=form.email.data, confirmed=False)
             db.session.add(email)
             db.session.commit()
-            flash('Thank you for your interest!', 'success')
+            flash('Thank you for your interest! Please check your inbox for confirmation email!', 'success')
             token = generate_confirmation_token(form.email.data)
             confirm_url = url_for('main.confirm_email', token=token, _external=True)
             html = render_template('email.html', confirm_url=confirm_url)
             subject = "Please confirm your subscription to ImgUpscale.com"
             send_email(form.email.data, subject, html)
 
-            flash('A confirmation email has been sent via email.', 'success')
-
-            return redirect(url_for('main.index'))
+        return redirect(url_for('main.index'))
     return render_template('main/index.html', form=form)
 
 @main_blueprint.route('/confirm/<token>')
@@ -53,4 +61,10 @@ def confirm_email(token):
         db.session.add(email_)
         db.session.commit()
         flash('You have confirmed your account! Thank you!')
+        # TODO send welcome email (create nice template)
     return redirect(url_for('main.index'))
+
+
+@main_blueprint.route('/privacy_policy')
+def privacy_policy():
+    return render_template('main/privacy_policy.html')
